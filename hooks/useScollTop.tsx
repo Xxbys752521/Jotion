9a901c70218c19d2023eaa3c4d9807bae02a7d53
xxbyswqz <Xxbys752521@gmail.com>
2023-10-21 17:05:53 +0800

    init project and build LandingPage

---
+++ hooks/useScollTop.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useScrollTop = (threshold = 10) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  function handleScroll() {
+    if (window.scrollY > threshold) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [threshold]);
+
+  return scrolled;
+};
+
+export default useScrollTop;
