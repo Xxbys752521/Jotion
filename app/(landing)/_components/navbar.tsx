1c679028b7f09317a68656827e9f359969707b40
xxbyswqz <Xxbys752521@gmail.com>
2023-10-21 21:24:03 +0800

    add convex as db and clerk as auth

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -5,8 +5,14 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div
@@ -17,6 +23,29 @@ const Navbar = () => {
     >
       <Logo />
       <div className=" md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Get Jotion Free
+              </Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain:"https://next-fish-75.clerk.accounts.dev",
+            applicationID:'convex'
+        }
+    ]
+}
